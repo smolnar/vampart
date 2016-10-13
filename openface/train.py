a2d6b7386cf7d87b35d1b524b3c1d2717712aef2
Samuel Molnar <molnar.samuel@gmail.com>
2016-10-13 21:36:26 +0200

    Add traning

--- openface/train.py
+++ openface/train.py
@@ -25,6 +25,8 @@ import itertools
 import os
 import numpy as np
 import json
+import re
+from contextlib import contextmanager
 
 np.set_printoptions(precision = 2)
 
@@ -36,45 +38,59 @@ dlibModelDir = os.path.join(modelDir, 'dlib')
 openfaceModelDir = os.path.join(modelDir, 'openface')
 dataFile = os.path.join(fileDir, './data.json')
 imagesPath = os.path.join(fileDir, 'storage/images')
+facesPath = os.path.join(fileDir, 'storage/faces')
 
 align = openface.AlignDlib(os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
-net = openface.TorchNeuralNet(os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'), 96)
+neuralNet = openface.TorchNeuralNet(os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'), 96)
 
-def getRep(path):
-    image = cv2.imread(path)
-
-    if image is None:
-        raise Exception("Unable to load image: {}".format(path))
-
-    rgbImage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    boxes = align.getAllFaceBoundingBoxes(rgbImage)
-
-    for box in boxes:
-        print("%s - %s" % x1, x2)
-
-    return
-
-    alignedFace = align.align(args.imgDim, rgbImg, bb, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
-
-    if alignedFace is None:
-        raise Exception("Unable to align image: {}".format(imgPath))
-    if args.verbose:
-        print("  + Face alignment took {} seconds.".format(time.time() - start))
-
+@contextmanager
+def measure(message):
+    print message, ' ...',
     start = time.time()
-    rep = net.forward(alignedFace)
-    if args.verbose:
-        print("  + OpenFace forward pass took {} seconds.".format(time.time() - start))
-        print("Representation:")
-        print(rep)
-        print("-----\n")
-    return rep
+    yield
+    print 'done ({}s)'.format(time.time() - start)
 
+def getFaces(path):
+    name = re.search('(\w+)\.(jpg|png)\Z', path).group(1)
+    faces = list()
+
+    with measure('Reading %s' % name):
+        image = cv2.imread(path)
+
+    with measure('Detecting faces'):
+        rgbImage = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        boxes = align.getAllFaceBoundingBoxes(rgbImage)
+
+    for i, box in enumerate(boxes):
+        x1, y1, x2, y2 = (box.left(), box.top(), box.right(), box.bottom())
+
+        if y2 - y1 >= 100:
+            start = time.time()
+            filename = '%s-%d.jpg' % (name, i)
+            face = image[y1:y2, x1:x2]
+            cv2.imwrite(os.path.join(facesPath, filename), face)
+
+            with measure('Extracting face as %s' % filename):
+                alignedFace = align.align(96, rgbImage, box, landmarkIndices = openface.AlignDlib.OUTER_EYES_AND_NOSE)
+
+                if alignedFace is not None:
+                    rep = neuralNet.forward(alignedFace)
+
+                    faces.append({
+                        'file': filename,
+                        'representation': rep.tolist()
+                    })
+
+    return faces
 
 with open(dataFile) as file:
     data = json.load(file)
 
     for artwork in data:
         imagePath = os.path.join(imagesPath, artwork['image'])
+        faces = getFaces(imagePath)
+        artwork['faces'] = faces
 
-        getRep(imagePath)
+    file.close()
+
+    json.dump(data, dataFile)
