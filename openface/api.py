80ff85caed48d2439287da4b345439b075e16834
Samuel Molnar <molnar.samuel@gmail.com>
2016-10-29 00:40:20 +0200

    Implement API for faces

--- openface/api.py
+++ openface/api.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import request
+from flask.json import jsonify
+from core import getFaces
+import os
 
 app = Flask(__name__)
+fileDir = os.path.dirname(os.path.realpath(__file__))
 
 @app.route('/')
 def getFacialModel():
-    pass
+    path = request.args.get('path')
+    imagePath = os.path.join(fileDir, path)
+    faces = getFaces(imagePath, save = False)
+
+    return jsonify(faces)
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 1337, threaded = true)
+    app.run(host = '0.0.0.0', port = 1337, threaded = True)
