8f371744f4cf93553e2102e204dd746c17d15ae9
Samuel Molnar <molnar.samuel@gmail.com>
2016-11-06 18:06:13 +0100

    Add feature specs

--- spec/features/image_upload_spec.rb
+++ spec/features/image_upload_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe 'Upload Image', type: :feature, js: true do
   before :each do
     visit root_path
     page.execute_script('$("#image_photo").attr("hidden", false);')
-    expect(Image.count).to eql(0)
   end
 
   it 'uploads an image with a face' do
@@ -12,12 +11,23 @@ RSpec.describe 'Upload Image', type: :feature, js: true do
 
     expect(page).to have_css('.face', count: 10)
     expect(Image.count).to eql(1)
+
+    visit image_path(Image.first.uid)
+
+    expect(page).to have_css('.face', count: 10)
   end
 
   it 'uploads an image without a face' do
     first('#image_photo').send_keys(Rails.root.join('spec/fixtures/mountains.jpeg'))
 
     expect(page).to have_content('Oh snap! We could not find a face in that picture. Be kind and provide another one, pretty please!')
+    expect(Image.count).to eql(0)
+  end
+
+  it 'uploads an image without a face' do
+    first('#image_photo').send_keys(Rails.root.join('spec/fixtures/file.txt'))
+
+    expect(page).to have_content('The uploaded image does not really look like an image, though. Hmm. Try another one, please!')
     expect(Image.count).to eql(0)
   end
 end
